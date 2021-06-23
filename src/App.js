82834f02341c646f48d144d8ada9075720708ce5
Nisal Renuja Palliyaguru <42801983+nisalrenuja@users.noreply.github.com>
2021-06-23 12:58:38 +0530

    form sign up added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import './App.css';
+import Form from './Form';
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello World</h1>
+      <Form />
     </div>
   );
 }
