55acbb8b66653716efa29dec294670febcd37a23
Nisal Renuja Palliyaguru <42801983+nisalrenuja@users.noreply.github.com>
2021-06-23 15:46:22 +0530

    image renamed

--- src/FormSuccess.js
+++ src/FormSuccess.js
@@ -5,7 +5,7 @@ const FormSuccess = () => {
   return (
     <div className='form-content-right'>
       <h1 className='form-success'>We have received your request!</h1>
-      <img className='form-img-2' src='img/form.png' alt='success-image' />
+      <img className='form-img-2' src='img/success.gif' alt='success-image' />
     </div>
   );
 };
